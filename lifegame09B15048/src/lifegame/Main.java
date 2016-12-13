eb0e151bacdf5a9e04700af05ea991d7f5c41b63
jeshandangol <jeshandangol@192.168.11.4>
2016-12-14 03:13:01 +0900

    Finished working with displaying gameboard,and living cells.
    window resizing is perfect. Next and Undo button yet to implement.

--- lifegame09B15048/src/lifegame/Main.java
+++ lifegame09B15048/src/lifegame/Main.java
@@ -1,8 +1,19 @@
 package lifegame;
 
-public class Main {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import java.awt.Dimension;
+
+
+public class Main implements Runnable{
 
 	public static void main(String[] args) {
+		
+		SwingUtilities.invokeLater(new Main());	
+	}
+	
+	public void run(){
 		
 		BoardModel model = new BoardModel(10,10);
 		model.addListener(new ModelPrinter());
@@ -30,6 +41,19 @@ public class Main {
 		model.changeCellState(4, 4);
 		System.out.println("d0ne");
 		
+		//create a new window
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation((JFrame.DISPOSE_ON_CLOSE));
+		
+		//create a base panel for the window
+		JPanel base = new JPanel();
+		frame.setContentPane(base);
+		base.setPreferredSize(new Dimension(400,300));
+		frame.setMinimumSize(new Dimension(300,200));
+		
+		frame.pack();
+		frame.setVisible(true);
+/*		
 		for(int i=1; i<12; ++i){
 			model.next();
 		}
@@ -41,6 +65,6 @@ public class Main {
 		
 			model.undo();
 		}
-		
+*/
 	}
 }
