c42c47a3a99ef208d9d651c1d8067e9aa3cf4dea
jeshandangol <jeshandangol@192.168.11.4>
2016-12-27 22:06:53 +0900

    Numerous updates:
    Tweaked windows resize and paint to cause smooth gameboard redrawing.
    
    Implemented BoardView under MouseListener etc.
    
    Added clearAll() in BoardModel for NewGame button.

--- lifegame09B15048/src/lifegame/BoardView.java
+++ lifegame09B15048/src/lifegame/BoardView.java
@@ -1,9 +1,14 @@
 package lifegame;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelListener;
+
 import java.awt.Graphics;
 import javax.swing.JPanel; 
 
-public class BoardView extends JPanel{
+public class BoardView extends JPanel implements MouseListener, MouseMotionListener{
 	
 	/**
 	 * 
@@ -14,6 +19,9 @@ public class BoardView extends JPanel{
 	
 	public BoardView(BoardModel model){
 		this.model = model;
+		//registering the listeners 
+		this.addMouseListener(this);
+		this.addMouseMotionListener(this);
 	}
 	
 	@Override								//use recommended
@@ -26,9 +34,9 @@ public class BoardView extends JPanel{
 		int X2, Y2;
 		
 		int defaultPadding = 15;			//padding on left, right and top sides of gameboard
-		int bottomPadding = 40;				//padding on the bottom side of gameboard
+		//int bottomPadding = 40;				//padding on the bottom side of gameboard
 		
-		int boardHeight = this.getHeight() -2*defaultPadding ; //- bottomPadding 
+		int boardHeight = this.getHeight() - 1*defaultPadding ; //- bottomPadding --previous was 2*defaultPadding 
 		int boardWidth = this.getWidth() - 2* defaultPadding;
 		
 //		size of rectangle cells to completely fill gameboard
@@ -40,18 +48,16 @@ public class BoardView extends JPanel{
 		
 //		define the starting and end points of gameboard, in each case of window size
 //		this calculation keeps gameboard centered on the window
-		if(boardWidth > boardHeight ){
-			X1 = (boardWidth - model.getCols() * cellUnit + 2*defaultPadding)/ 2;
-			Y1 = defaultPadding;
-			X2 = X1 + model.getCols() * cellUnit;
-			Y2 = defaultPadding + model.getRows() * cellUnit;
-		}
-		else{
-			X1 = defaultPadding;
-			Y1 = (boardHeight - model.getRows() * cellUnit ) / 2;
-			X2 = defaultPadding + model.getCols() * cellUnit;
-			Y2 = Y1 + model.getRows() * cellUnit;
-		}
+		/*starting coordinates: default Padding + half of the gap resulting
+		 * due to adopting smaller side of other dimension(height/width) as cellUnit
+		 * for the dimension whose side is adopted as cellUnit, X1 or Y1 reduces to simple defaultPadding
+		 * 
+		 * when cellHeight = cellWidth, X1=Y1= defaultPadding, making the resize of gameboard smooth.		
+		 */
+		X1 = defaultPadding + (boardWidth - model.getCols() * cellUnit)/ 2;
+		Y1 = defaultPadding + (boardHeight - model.getRows() * cellUnit) / 2;//defaultPadding;
+		X2 = X1 + model.getCols() * cellUnit;
+		Y2 = Y1 + model.getRows() * cellUnit;
 		
 //		draw horizontal lines
 		for(int j= Y1; j<= Y2; j+= cellUnit  ){
@@ -74,6 +80,48 @@ public class BoardView extends JPanel{
 					g.fillRect( X1 + (j)*cellUnit , Y1 + (i)*cellUnit, cellUnit, cellUnit);
 			}
 		}
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 	
